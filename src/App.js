6b40ad8b8fcd99a9a9a387074aadb469739df0e8
Matt Wilson <slaterduskinwilson@gmail.com>
2021-10-31 17:53:34 -0500

    im in!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi Matt!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hi Matt!</h1>
+            <h6>eyyyyyyyyyy</h6>
+        </div>
+    )
 }
 
-export default App;
+export default App
