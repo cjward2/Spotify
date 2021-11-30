bfdb4475df6f6f579c63d068d9873fee263b4b3f
Matt Wilson <slaterduskinwilson@gmail.com>
2021-11-30 17:21:50 -0600

    im at about 1:27 in the tutorial. so far the user can log in, and the app will display their spotify access token. cool

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
+import { useEffect, useState } from 'react'
 import Login from './components/Login'
 import './App.css'
+import { getTokenFromUrl } from './spotify'
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const spotify = new SpotifyWebApi() //this is a SUPER OBJECT responsible for any interaction between Spotify and our React
 
 function App() {
+    const [token, setToken] = useState(null)
+
+    useEffect(() => {
+        //useEffect will run any time the window loads
+        const hash = getTokenFromUrl()
+        window.location.hash = ''
+        //next step: remove the token from the url so it doesn't show up in the user's bar
+        const _token = hash.access_token
+        // this gives us just the token string itself and not its expiration date, etc
+        //it's convention to use underscore for a quick temp variable name
+
+        if (_token) {
+            setToken(_token)
+            // if we have a token, set the token state to be that token!
+            spotify.setAccessToken(_token)
+            // once we plug this key in, react and spotify can talk freely with each other. We're now Logged In.
+            spotify.getMe().then(user => {
+                console.log('This is the user, if they exist:', user)
+                //the getMe() function gives us info about our user account, it returns a promise
+                //for some reason, using a template literal on the console log for user just gives me object:Object or whatever. why is this?
+            })
+        }
+
+        console.log('BAAAAA I HAVE A TOKEN! ', _token)
+    }, [])
+    //the empty array as a second argument in useEffect is significant. Why?
+    //
+    //awesome! now, when we load our page, we get the token object from the url!
+    //check the spotify script to see how
+
     return (
         <div className="App">
-            <Login />
+            {token ? <h1>OH CRAP! IM LOGGED IN! {token}</h1> : <Login />}
         </div>
     )
+    // ternary: if token, show the player! if not? show the login!
 }
 
 export default App
--- src/spotify.js
+++ src/spotify.js
@@ -3,8 +3,10 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize'
 // this is where it takes you when you click the login button
 
-const redirectUri = 'http://localhost:/3000'
+const redirectUri = 'http://localhost:3000/'
 //once logged in, redirect to the address of our app, which is currently running on localhost
+//CAREFUL! YOU NEED THIS TRAILING SLASH, OTHERWISE THE WHOLE FUCKING THING BREAKS! who woulda thought?
+//don't omit the slash after :3000
 
 const clientId = 'fe676df031a148faa30ea0b8fc4737e5'
 
@@ -17,8 +19,32 @@ const scopes = [
     'user-modify-playback-state'
 ]
 
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirectUri=${redirectUri}&scopre=${scopes.join(
+export const getTokenFromUrl = () => {
+    //we're creating this function ourselves!
+    //video at 1:01:46 at this point...
+    return window.location.hash
+        .substring(1)
+        .split('&') //split at the ampersand character
+        .reduce((initial, item) => {
+            //but what the shit is reduce?
+            //then split again at the equal sign to break off the secret access token
+            let parts = item.split('=')
+            initial[parts[0]] = decodeURIComponent(parts[1])
+            //go into the initial array that's being returned, for the access token decode the URI component
+            return initial
+
+            //all we need to really know here is that this code pulls the access token out
+        }, {})
+
+    //this goes to the has # in the url (that's what window.location is, the url)
+    // my url at this point is:
+    // http://localhost:3000/#access_token=BQB0khbhVLtEaWifOXFrjvI3zcoKt8Mk2kTdFKFlUgAomI23zBIus1mGVY3l96LDyqVktAwLNlwd5H_WVDKwKOHmiu1510mpHPCcS9jYm777VupxAbMxj81SXBe1jTLfjBCoAT5wzSRx5DiVSJzskWzoL6iYY6mVHfmpb2iWmiJLXsriJw&token_type=Bearer&expires_in=3600
+    //see where the "hash" is? after 3000/
+}
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
     '%20'
 )}&response_type=token&show_dialog=true`
 
 // %20 is the ascii key for a space, so we're putting spaces between all our scopes
+// this is how we're able to pass all these different scope strings to our url... they're &scope=blah-blah%20fuck-shit-fuck%20go-to-hell
